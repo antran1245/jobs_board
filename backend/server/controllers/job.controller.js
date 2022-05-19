274aedddec7704b1f6c079e19857b2a4843df573
antran1245 <antran1245@gmail.com>
2022-05-18 22:50:07 -0700

    completed controllers and config

--- backend/server/controllers/job.controller.js
+++ backend/server/controllers/job.controller.js
@@ -7,5 +7,31 @@ module.exports.index = (req, res) => {
 }
 
 module.exports.createJob = (req, res) => {
-    
+    const { title, company, salary, remote } = req.body;
+    Jobs.create({
+        title,
+        company,
+        salary,
+        remote
+    })
+    .then(job => res.json(job))
+    .catch(err => res.json(err))
+}
+
+module.exports.findAllJobs = (req, res) => {
+    Jobs.find()
+    .then(jobs => res.json(jobs))
+    .catch(err => res.json(err))
+}
+
+module.exports.updateJob = (req, res) => {
+    Jobs.findOneAndUpdate({_id: req.params._id})
+    .then(job => res.json(job))
+    .catch(err => res.json(err))
+}
+
+module.exports.deleteJob = (req, res) => {
+    Jobs.deleteOne({_id: req.params._id})
+    .then(deleted => res.json(deleted))
+    .catch(err => res.json(err))
 }
